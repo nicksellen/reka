71858eacc37cf8b0fad45981b66d0bfe2c34d7a7
Nick Sellen <git@nicksellen.co.uk>
2014-09-15 12:10:34 -0700

    added path option

--- main/pl.lua
+++ main/pl.lua
@@ -1,9 +1,9 @@
 init = function(args)
    wrk.init(args)
-   depth = tonumber(args[1]) or 1
+   depth = tonumber(args[2]) or 1
    local r = {}
    for i=1,depth do
-      r[i] = wrk.format(nil, "/plaintext")
+      r[i] = wrk.format(nil, args[1] or "/")
    end
    req = table.concat(r)
 end
